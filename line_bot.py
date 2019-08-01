f93bbaa4490609d7f9df7e15ecfa170c944713c6
Error Handle <mazlol0311@gmail.com>
2019-08-01 18:44:39 +0800

    fix requirements.txt

--- line_bot.py
+++ line_bot.py
@@ -38,10 +38,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '你在說啥'
+    r = ''
     if msg == 'hi':
         r = 'hi'
-    elif msg == '你吃飯了嗎?'
+    elif msg == '你吃飯了嗎'
         r = 'not yet'
 
     line_bot_api.reply_message(
