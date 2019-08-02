4bcf7b546a2a5116266d2d1086e409a46ebda1c5
Error Handle <mazlol0311@gmail.com>
2019-08-02 14:48:02 +0800

    test

--- line_bot.py
+++ line_bot.py
@@ -41,7 +41,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='hi' + profile.display_name))
+        TextSendMessage(text='hi'))
 
 
 if __name__ == "__main__":
