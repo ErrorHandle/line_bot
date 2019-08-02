64aa892775be7fb4ae8b7dc93af58e4afe2773a8
Error Handle <mazlol0311@gmail.com>
2019-08-02 14:42:18 +0800

    add user name

--- line_bot.py
+++ line_bot.py
@@ -37,9 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    profile = line_bot_api.get_profile(user_id)
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='不要問'))
+        TextSendMessage(text='hi' + profile.display_name))
 
 
 if __name__ == "__main__":
